5964c2243c75a3509c32d27343a6961e7cd11035
Pastitas <Pastitas@users.noreply.github.com>
2020-08-06 20:13:29 +0200

    toqueteando nvim, terminator y haciendo mejor los install scripts

--- terminal/zsh/install.sh
+++ terminal/zsh/install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+OHMYDIR="$HOME/.oh-my-zsh"
 
 dotfiles_install_package zsh
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" || true
 
 # Install fishshell completions
-dotfiles_install_remote_component GITHUB tarruda/zsh-autosuggestions fish-completions/zsh-autosuggestions
-dotfiles_install_remote_component GITHUB jimmijj/zsh-syntax-highlighting fish-completions/zsh-syntax-highlighting
+dotfiles_install_remote_component GITHUB tarruda/zsh-autosuggestions $OHMYDIR/fish-completions/zsh-autosuggestions
+dotfiles_install_remote_component GITHUB jimmijj/zsh-syntax-highlighting $OHMYDIR/fish-completions/zsh-syntax-highlighting
 
 # Install config
 dotfiles_install_component .zshrc $HOME/.zshrc
