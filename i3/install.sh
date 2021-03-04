d85e043b602feca6d1a14fe8a81aac3fdff53fb4
pastitas <Pastita@users.noreply.github.com>
2021-03-04 15:21:36 +0100

    aligned with master

--- i3/install.sh
+++ i3/install.sh
@@ -1,22 +1,10 @@
-
+current_dir=${DOTFILES_CURRENT_SOURCE_DIR}
 doti3=$HOME/.config/i3
 fonts=$HOME/.fonts
 
-if [ ! -d $doti3 ]; then
-    mkdir $doti3
-fi
 if [ ! -d $fonts ]; then
     mkdir $fonts
 fi
 
 dotfiles_install_package i3 i3status dmenu rofi
-dotfiles_install_component config $doti3/config
-dotfiles_install_component i3status.conf $doti3/i3status.conf
-dotfiles_install_component conky.conf $doti3/conky.conf
-dotfiles_install_component fonts $fonts
-dotfiles_install_component i3exit $doti3/i3exit
-dotfiles_install_component workstation $doti3/workstation
-dotfiles_install_component laptop $doti3/laptop
-dotfiles_install_component mirror $doti3/mirror
-
-
+dotfiles_install_component $current_dir $doti3
